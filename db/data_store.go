f391438154751479823b36b9b4bf9489580fa0d4
IDOMATH <btdothemath@gmail.com>
2024-12-05 20:23:13 -0600

    Adding post data handler

--- db/data_store.go
+++ db/data_store.go
@@ -21,14 +21,14 @@ func NewDataStore(db *sql.DB) *DataStore {
 	}
 }
 
-func (s *DataStore) InsertData(entry Entry) (int, error) {
+func (s *DataStore) InsertData(entry string) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	var newId int
 	statement := `insert into data $1`
 
-	err := s.Db.QueryRowContext(ctx, statement, entry.Data).Scan(&newId)
+	err := s.Db.QueryRowContext(ctx, statement, entry).Scan(&newId)
 
 	if err != nil {
 		return 0, err
--- main.go
+++ main.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/IDOMATH/docker-example/db"
 )
+
+var DS db.DataStore
 
 func main() {
 	fmt.Println("Hello world")
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /", handleHome)
+	router.HandleFunc("POST /", handlePostData)
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
+
+	connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", "localhost", "5432", "postgres", "postgres", "mysecretpassword", "disable")
+	postgresDb, err := db.ConnectSql(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	DS = *db.NewDataStore(postgresDb.Sql)
 
 	fmt.Println("Starting on port 8080")
 	log.Fatal(server.ListenAndServe())
@@ -23,4 +36,9 @@ func main() {
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome Home"))
+}
+
+func handlePostData(w http.ResponseWriter, r *http.Request) {
+	DS.InsertData(time.Now().String())
+	w.Write([]byte("Entered"))
 }
